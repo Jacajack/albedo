886ce0bf3fd404b012eda88e775a5eb9d91744d5
Jacek Wieczorek <mrjjot@gmail.com>
2020-04-19 22:47:57 +0200

    That one too, oops

---
+++ include/albedo/gl/gl.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+/**
+	\file This file only ensures that all necessary GL headers
+	are included in the right order
+*/
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
