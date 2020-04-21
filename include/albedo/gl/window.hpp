4e3ed3f5abc198609210b95ed21e0d16776ba5e2
Jacek Wieczorek <mrjjot@gmail.com>
2020-04-21 22:16:06 +0200

    Add window builder class

--- include/albedo/gl/window.hpp
+++ include/albedo/gl/window.hpp
@@ -10,6 +10,8 @@
 namespace abd {
 namespace gl {
 
+class window_builder;
+
 /**
     A wrapper for GLFWwindow
 */
@@ -23,7 +25,7 @@ public:
 		glfwWindowHint(h.first, h.second);
 	}
 
-	window(int resx, int resy, const std::string &title, GLFWmonitor *monitor = nullptr, GLFWwindow *share = nullptr, std::initializer_list<hint> hints = {});
+	window(int resx, int resy, const std::string &title, const window_builder &builder);
 	virtual ~window() = default;
 
 	// Copy ctor and assignment operator
@@ -57,6 +59,78 @@ public:
 
 private:
 	std::unique_ptr<GLFWwindow, void (*)(GLFWwindow *)> m_window;
+};
+
+/**
+	A builder class for the window
+*/
+class window_builder
+{
+public:
+	window_builder &context_version(int major, int minor)
+	{
+		m_minor = minor;
+		m_major = major;
+		return *this;
+	}
+
+	window_builder &debug(bool enable)
+	{
+		m_debug = enable;
+		return *this;
+	}
+
+	window_builder &forward_compat(bool enable)
+	{
+		m_forward_compat = enable;
+		return *this;
+	}
+
+	window_builder &profile(int profile)
+	{
+		m_profile = profile;
+		return *this;
+	}
+
+	window_builder &samples(int n)
+	{
+		m_samples = n;
+		return *this;
+	}
+
+	window_builder &monitor(GLFWmonitor *mon)
+	{
+		m_monitor = mon;
+		return *this;
+	}
+
+	GLFWmonitor *get_monitor() const
+	{
+		return m_monitor;
+	}
+
+	window_builder &parent_window(GLFWwindow *win)
+	{
+		m_parent = win;
+		return *this;
+	}
+
+	GLFWwindow *get_parent_window() const
+	{
+		return m_parent;
+	}
+
+	std::vector<window::hint> to_hints() const;
+
+private:
+	int m_major = 4;
+	int m_minor = 5;
+	bool m_forward_compat = true;
+	bool m_debug = false;
+	int m_samples = 0;
+	int m_profile = GLFW_OPENGL_CORE_PROFILE;
+	GLFWmonitor *m_monitor = nullptr;
+	GLFWwindow *m_parent = nullptr;
 };
 
 }
--- src/gl/window.cpp
+++ src/gl/window.cpp
@@ -2,16 +2,22 @@
 #include <albedo/exception.hpp>
 
 using abd::gl::window;
+using abd::gl::window_builder;
 
-window::window(int resx, int resy, const std::string &title, GLFWmonitor *monitor, GLFWwindow *share, std::initializer_list<hint> hints)
-	: m_window(nullptr, nullptr)
+
+window::window(int resx, int resy, const std::string &title, const window_builder &builder) :
+	m_window(nullptr, nullptr)
 {
 	// Set all hints
 	glfwDefaultWindowHints();
-	for (const auto &h : hints)
+	for (const auto &h : builder.to_hints())
 		window::set_global_hint(h);
 
-	m_window = decltype(m_window)(glfwCreateWindow(resx, resy, title.c_str(), monitor, share), glfwDestroyWindow);
+	m_window = decltype(m_window){glfwCreateWindow(resx,
+		resy,
+		title.c_str(),
+		builder.get_monitor(),
+		builder.get_parent_window()), glfwDestroyWindow};
 
 	if (!m_window)
 		throw abd::exception("failed to create GLFW window");
@@ -35,4 +41,16 @@ bool window::should_close() const
 void window::swap_buffers()
 {
 	return glfwSwapBuffers(m_window.get());
+}
+
+std::vector<window::hint> window_builder::to_hints() const
+{
+	return std::vector<window::hint>{
+		{GLFW_OPENGL_FORWARD_COMPAT, m_forward_compat},
+		{GLFW_OPENGL_DEBUG_CONTEXT, m_debug},
+		{GLFW_CONTEXT_VERSION_MAJOR, m_major},
+		{GLFW_CONTEXT_VERSION_MINOR, m_minor},
+		{GLFW_OPENGL_PROFILE, m_profile},
+		{GLFW_SAMPLES, m_samples},
+	};
 }
