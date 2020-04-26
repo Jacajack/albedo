d3f44604600c80b713dbcf32ced69ba9fcd19aec
Jacek Wieczorek <mrjjot@gmail.com>
2020-04-26 22:33:10 +0200

    Move uniform and uniform_block into separate file

--- include/albedo/gl/program.hpp
+++ include/albedo/gl/program.hpp
@@ -3,6 +3,7 @@
 #include <albedo/gl/gl.hpp>
 #include <albedo/gl/gl_object.hpp>
 #include <albedo/gl/shader.hpp>
+#include <albedo/gl/uniform.hpp>
 #include <initializer_list>
 #include <string>
 #include <map>
@@ -10,88 +11,10 @@
 namespace abd {
 namespace gl {
 
-class program;
-
 /**
-    Represents an OpenGL program uniform.
-    \warning Lifetime of an uniform class may not exceed lifetime of the program class it was retrieved from.
-
-    \todo add type checking and functions for providing uniform values
-*/
-class uniform : abd::noncopy
-{
-	friend class program;
-
-public:
-	inline GLuint get_location() const;
-	inline GLenum get_type() const;
-	inline GLint get_size() const;
-	inline GLint get_block_index() const;
-
-	inline std::size_t get_field_offset(int column, int row = 0);
-
-private:
-	uniform(
-		const gl::program &program,
-		GLint location,
-		GLenum type,
-		GLint size,
-		GLint block_index,
-		GLint offset,
-		GLint array_stride,
-		GLint matrix_stride,
-		GLint is_row_major);
-
-	GLuint m_program;
-	GLint m_location;
-	GLenum m_type;
-	GLint m_size;
-	GLint m_block_index;
-	GLint m_offset;
-	GLint m_array_stride;
-	GLint m_matrix_stride;
-	bool m_is_row_major;
-};
-
-/**
-    Returns the uniform location
-*/
-GLuint uniform::get_location() const
-{
-	return m_location;
-}
-
-/**
-    Returns offset of an array/matrix field
-*/
-std::size_t uniform::get_field_offset(int column, int row)
-{
-	if (!m_is_row_major)
-		return m_offset + row * m_array_stride + column * m_matrix_stride;
-	else
-		return m_offset + column * m_array_stride + row * m_matrix_stride;
-}
-
-/**
-    Represents an OpenGL program uniform block
-    \warning Lifetime of an uniform_block class may not exceed lifetime of the program class it was retrieved from.
-*/
-class uniform_block : abd::noncopy
-{
-	friend class program;
-
-public:
-	GLint get_location() const;
-
-private:
-	uniform_block(const gl::program &program, GLuint location);
-
-	GLuint m_program;
-	GLuint m_index;
-};
-
-/**
-    Represents RAII wrapper for an OpenGL program object
+    Represents RAII wrapper for an OpenGL program object. The RAII here isn't quite perfect, because
+	you have to take care to only use uniforms and uniform_blocks acquired from this program when it is
+	used itself.
 */
 class program : public abd::gl::gl_object<abd::gl::gl_object_type::PROGRAM>
 {
